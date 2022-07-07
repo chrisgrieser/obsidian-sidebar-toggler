494c4c4f66ad49c7f076b3b446db86388d1bd127
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2022-07-07 22:37:19 +0200

    URI schemes working

--- main.ts
+++ main.ts
@@ -1,4 +1,9 @@
-import { Plugin } from "obsidian";
+import { Notice, Plugin } from "obsidian";
+
+interface UriParameters {
+	side?: string;
+	show?: string;
+}
 
 export default class SidebarToggler extends Plugin {
 
@@ -26,18 +31,38 @@ export default class SidebarToggler extends Plugin {
 			callback: () => this.showSidebar("right", false),
 		});
 
+		this.registerObsidianProtocolHandler("sidebar", async (p) => {
+			// 🏴‍☠️ ryan's log plugin
+			const parameters = p as unknown as UriParameters;
+			for (const parameter in parameters) {
+				(parameters as any)[parameter] = (parameters as any)[parameter];
+			}
+
+			const sideParamValid = ["right", "left"].includes(parameters.side);
+			const showParamValid = ["true", "false"].includes(parameters.show);
+			if (!sideParamValid || !showParamValid) {
+				new Notice ("URI invalid.\n\nPlease refer to the README of the Sidebar Toggler Plugin.");
+				return;
+			}
+
+			const show = parameters.show === "true";
+			this.showSidebar(parameters.side, show);
+			console.log("Side: " + parameters.side);
+			console.log("Show: " + show);
+		});
+
 	}
 
 	async onunload() { console.log("Sidebar Toggler Plugin unloaded.") }
 
 	showSidebar (side: string, show: boolean) {
-		if (side === "left") {
-			if (show) this.app.workspace.leftSplit.expand();
-			else this.app.workspace.leftSplit.collapse();
-		} else if (side === "right") {
-			if (show) this.app.workspace.rightSplit.expand();
-			else this.app.workspace.rightSplit.collapse();
-		}
+		const leftHidden = this.app.workspace.leftSplit.collapsed;
+		const rightHidden = this.app.workspace.rightSplit.collapsed;
+
+		if (side === "left" && show && leftHidden) this.app.workspace.leftSplit.expand();
+		else if (side === "right" && show && rightHidden) this.app.workspace.rightSplit.expand();
+		else if (side === "left" && !show && !leftHidden) this.app.workspace.leftSplit.collapse();
+		else if (side === "right" && !show && !leftHidden) this.app.workspace.rightSplit.collapse();
 	}
 
 }
